ce429fed2f9afe66cce14f45298cf6a198c52575
Ambooken-LinuxDev-004 <joeambooken04@gmail.com>
2025-07-13 04:38:37 +0530

    Need to integrate Servo & Ultrasonic and Motor Driver Code(didn't sink)

--- Mini-Project-Arduino/src/main.cpp
+++ Mini-Project-Arduino/src/main.cpp
@@ -1,7 +1,17 @@
 #include <Arduino.h>
+//#include <Servo.h>
+
+//Servo myservo;
+int angle = 140;
+
 // IR sensor pins
-const int leftIR = 12;
-const int rightIR = 11;
+//const int leftIR = 12;
+//const int rightIR = 11;
+
+// Ultrasonic sensor pins
+int trigPin = 11;
+int echoPin = 12;
+
 
 // Motor driver pins
 const int IN1 = 7;
@@ -11,14 +21,38 @@ const int IN4 = 10;
 
 void setup() {
   // IR sensor inputs
-  pinMode(leftIR, INPUT);
-  pinMode(rightIR, INPUT);
+  //pinMode(leftIR, INPUT);
+  //pinMode(rightIR, INPUT);
+
+  //myservo.attach(3);
+  //myservo.write(angle);
+  //delay(500);
+
+  // Ultrasonic values
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
 
   // Motor outputs
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+long gettingDistance() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  long duration = pulseIn(echoPin, HIGH);
+  long distance = duration * 0.034 / 2; // Speed of sound: 343 m/s
+  return distance;
 }
 
 void moveForward() {
@@ -66,8 +100,58 @@ void stopMotors() {
 }
 
 void loop() {
-  int leftSensor = digitalRead(leftIR);
+
+/*
+  for(angle=180; angle>=80; angle-=1){
+    long distance = gettingDistance();
+    if(distance > 20)
+    {
+      moveForward();
+    }
+    else
+    {
+      stopMotors();
+    }
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    myservo.write(angle);
+    delay(100);
+  }
+  for(angle=80; angle<=180; angle+=1){
+    long distance = gettingDistance();
+    if(distance > 20)
+    {
+      moveForward();
+    }
+    else
+    {
+      stopMotors();
+    }
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+    myservo.write(angle);
+    delay(100);
+  }
+  */
+
+  long distance = gettingDistance();
+  if(distance > 20)
+    {
+      moveForward();
+      Serial.println("Moving forward...");
+    }
+    else
+    {
+      stopMotors();
+      Serial.println("Stopped...");
+    }
+
+
+  /*int leftSensor = digitalRead(leftIR);
   int rightSensor = digitalRead(rightIR);
+
 
   // Logic: Black line = 0, White = 1
   if (leftSensor == 1 && rightSensor == 1) {
@@ -87,7 +171,7 @@ void loop() {
     // Both on white - stop or move forward (depends on design)
     //stopMotors();
     moveForward();
-  }
+  }*/
 }
 
 
